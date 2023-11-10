ba4696c78083f4755a9f8c81ac647c93472d1ea5
Maria G <mgorban@rheinahrcampus.de>
2023-11-10 17:41:25 +0100

    add task01 solution

---
+++ src/homework26/task01/Main.java
@@ -0,0 +1,17 @@
+package homework26.task01;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Human01 andrii = new Human01("Андрій", 22);
+    andrii.introduce();
+
+    Human01 maria = new Human01("Марійка", 21);
+    maria.introduce();
+
+    Human01 veronika = new Human01("Вероніка", 20);
+    veronika.introduce();
+
+  }
+
+}
