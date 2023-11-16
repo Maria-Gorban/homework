acc0f7c0d195c3fc6e45971f37f50a417b24a957
Maria G <mgorban@rheinahrcampus.de>
2023-11-16 20:48:06 +0100

    add task01 solution

--- src/homework28/Main.java
+++ src/homework28/Main.java
@@ -1,8 +1,8 @@
 package homework28;
 
 
-
 public class Main {
+
   public static void main(String[] args) {
     double[] arr = {4.02, 2, 77, 1, -5.25, 600};
     MyArray myArray = new MyArray(arr);
@@ -13,11 +13,10 @@ public class Main {
     myArray.remove(myArray.indexOf(7));
     System.out.println("Массив после урока: " + myArray);
 
-
     System.out.println("В нашем массиве есть число 22? " + myArray.contains(22));
     System.out.println("В нашем массиве есть число 123? " + myArray.contains(123));
 
-    double[] arr2 = { };
+    double[] arr2 = {};
     MyArray myArray2 = new MyArray(arr2);
     System.out.println("Массив myArray пустой? " + myArray.isEmpty());
     System.out.println("Массив myArray2 пустой? " + myArray2.isEmpty());
@@ -32,7 +31,7 @@ public class Main {
     System.out.println("Заменим 2 на -22.2: " + myArray);
 
     System.out.println("Добавим пару ненужных нулей");
-    for (int i = 0; i < myArray.size(); i+=2) {
+    for (int i = 0; i < myArray.size(); i += 2) {
       myArray.add(i, 0.0);
     }
     System.out.println(myArray);
@@ -41,11 +40,6 @@ public class Main {
     System.out.println("Удалим первый 0\n" + myArray);
     myArray.removeAll(0.0);
     System.out.println("А теперь удалим все нули\n" + myArray);
-
-
-
-
-
 
 
   }
