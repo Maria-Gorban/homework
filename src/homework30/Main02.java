c2a70326e28691a03a2fb5c6ecc8997df711e210
Maria G <mgorban@rheinahrcampus.de>
2023-11-18 12:00:36 +0100

    add task02 solution

---
+++ src/homework30/Main02.java
@@ -0,0 +1,14 @@
+package homework30;
+
+public class Main02 {
+
+  public static void main(String[] args) {
+    int a = -45;
+    double b = 34.013;
+    short c = 128;
+    char d = 'P';
+
+    System.out.println(a*(b-d)+c*a/d);
+  }
+
+}
