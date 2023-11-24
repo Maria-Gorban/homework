fd6a07469eed8091e394a926dcc1f447683498b4
Maria G <mgorban@rheinahrcampus.de>
2023-11-24 18:25:08 +0100

    fix task02

--- src/homework31/Main02.java
+++ src/homework31/Main02.java
@@ -9,26 +9,23 @@ public class Main02 {
 
     Random random = new Random();
     Scanner scanner = new Scanner(System.in);
-    while(true) {
-      System.out.println("Сколько костей кидаем? 1 или 2");
-      int answer = scanner.nextInt();
-      int a;
-      int b;
+    System.out.println("Сколько костей кидаем? 1 или 2");
+    int answer = scanner.nextInt();
+    int a;
+    int b;
+    int min = 1;
+    int max = 6;
 
-      switch (answer) {
-        case 1:
-          a = 1 + random.nextInt(6 );
-          System.out.println("Вы выкинули: " + a);
-          break;
-        case 2:
-          a = 1 + random.nextInt(6 );
-          b = 1 + random.nextInt(6 );
-          System.out.println("Вы выкинули: " + a + " и " + b);
-          break;
-        default:
-          System.out.println("Такого количества костей у меня нет.");
-          return;
-      }
+    if (answer == 1) {
+      a = min + random.nextInt(max - min + 1);
+      System.out.println("Вы выкинули: " + a);
+    } else if (answer == 2) {
+      a = min + random.nextInt(max - min + 1);
+      b = min + random.nextInt(max - min + 1);
+      System.out.println("Вы выкинули: " + a + " и " + b);
+    } else {
+      System.out.println("Такого количества костей у меня нет.");
     }
   }
 }
+
