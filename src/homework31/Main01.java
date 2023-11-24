9497b21cdef32d378cf55b083d4f548719b9000f
Maria G <mgorban@rheinahrcampus.de>
2023-11-24 18:21:01 +0100

    fix task01

--- src/homework31/Main01.java
+++ src/homework31/Main01.java
@@ -9,32 +9,19 @@ public class Main01 {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    System.out.print("Введите любое число типа int: ");
+    int a = scanner.nextInt();
 
-    int i = 0;
-    int a = 0;
-    double b = 0;
-    short c = 0;
-    float d = 0;
+    System.out.print("Введите любое число типа double: ");
+    double b = scanner.nextDouble();
 
-    while (i < 4) {
-      System.out.print("Введите какое то число: ");
-      i += 1;
-      switch (i) {
-        case 1:
-          a = scanner.nextInt();
-          break;
-        case 2:
-          b = scanner.nextDouble();
-          break;
-        case 3:
-          c = scanner.nextShort();
-          break;
-        case 4:
-          d = scanner.nextFloat();
-          break;
-      }
+    System.out.print("Введите любое число типа short: ");
+    short c = scanner.nextShort();
 
-    }
+    System.out.print("Введите любое число типа float: ");
+    float d = scanner.nextFloat();
+
+
     double result = (a * (b - d) + c * a / d);
     System.out.println(a + " * (" + b + " - " + d + ") + "+ c + " * " + a + " / " + d + " = " + result);
 
