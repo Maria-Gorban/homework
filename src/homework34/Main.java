8c34a6ef0c804423a5febc22c80e7ff680e0ddbb
Maria G <mgorban@rheinahrcampus.de>
2023-11-29 17:42:40 +0100

    fix task02

--- src/homework34/Main.java
+++ src/homework34/Main.java
@@ -4,57 +4,48 @@ import java.util.Scanner;
 
 public class Main {
 
+  public static void printInfo(Planets planet) {
+    System.out.println("Радиус: " + planet.getRadius() + " км");
+    System.out.println("Масса: " + planet.getWeight() + " кг");
+    System.out.println(
+        "Ускорение свободного падения: " + planet.printAccelerationOfGravity() + " м/сек^2");
+  }
+
   public static void printTyp(int n) {
     switch (n) {
       case 1:
         System.out.println(Planets.MERCURY.getName()
-            + ", планета земного типа самая близкая к Солнцу, необитаема");
-        System.out.println("Радиус: " + Planets.MERCURY.getRadius() + " км");
-        System.out.println("Масса: " + Planets.MERCURY.getWeight() + " кг");
-        System.out.println("Ускорение свободного падения: " + Planets.MERCURY.printAccelerationOfGravity() + " м/сек^2");
+            + ", планета земного типа, самая близкая к Солнцу, необитаема");
+        printInfo(Planets.getByNumber(1));
         break;
       case 2:
         System.out.println(Planets.VENUS.getName() + ", планета земного типа, необитаема");
-        System.out.println("Радиус: " + Planets.VENUS.getRadius() + " км");
-        System.out.println("Масса: " + Planets.VENUS.getWeight() + " кг");
-        System.out.println("Ускорение свободного падения: " + Planets.VENUS.printAccelerationOfGravity() + " м/сек^2");
+        printInfo(Planets.getByNumber(2));
         break;
       case 3:
         System.out.println(Planets.EARTH.getName() + ", планета земного типа, обитаема");
-        System.out.println("Радиус: " + Planets.EARTH.getRadius() + " км");
-        System.out.println("Масса: " + Planets.EARTH.getWeight() + " кг");
-        System.out.println("Ускорение свободного падения: " + Planets.EARTH.printAccelerationOfGravity() + " м/сек^2");
+        printInfo(Planets.getByNumber(3));
         break;
       case 4:
         System.out.println(Planets.MARS.getName() + ", планета земного типа, необитаема");
-        System.out.println("Радиус: " + Planets.MARS.getRadius() + " км");
-        System.out.println("Масса: " + Planets.MARS.getWeight() + " кг");
-        System.out.println("Ускорение свободного падения: " + Planets.MARS.printAccelerationOfGravity() + " м/сек^2");
+        printInfo(Planets.getByNumber(4));
         break;
       case 5:
         System.out.println(Planets.JUPITER.getName() + ", газовый гигант, необитаема");
-        System.out.println("Радиус: " + Planets.JUPITER.getRadius() + " км");
-        System.out.println("Масса: " + Planets.JUPITER.getWeight() + " кг");
-        System.out.println("Ускорение свободного падения: " + Planets.JUPITER.printAccelerationOfGravity() + " м/сек^2");
+        printInfo(Planets.getByNumber(5));
         break;
       case 6:
         System.out.println(Planets.SATURN.getName() + ", газовый гигант, необитаема");
-        System.out.println("Радиус: " + Planets.SATURN.getRadius() + " км");
-        System.out.println("Масса: " + Planets.SATURN.getWeight() + " кг");
-        System.out.println("Ускорение свободного падения: " + Planets.SATURN.printAccelerationOfGravity() + " м/сек^2");
+        printInfo(Planets.getByNumber(6));
         break;
       case 7:
         System.out.println(Planets.URANUS.getName() + ", ледяной гигант, необитаема");
-        System.out.println("Радиус: " + Planets.URANUS.getRadius() + " км");
-        System.out.println("Масса: " + Planets.URANUS.getWeight() + " кг");
-        System.out.println("Ускорение свободного падения: " + Planets.URANUS.printAccelerationOfGravity() + " м/сек^2");
+        printInfo(Planets.getByNumber(7));
         break;
       case 8:
         System.out.println(Planets.NEPTUNE.getName()
             + ", самая дальняя планета от Солнца, ледяной гигант, необитаема");
-        System.out.println("Радиус: " + Planets.NEPTUNE.getRadius() + " км");
-        System.out.println("Масса: " + Planets.NEPTUNE.getWeight() + " кг");
-        System.out.println("Ускорение свободного падения: " + Planets.NEPTUNE.printAccelerationOfGravity() + " м/сек^2");
+        printInfo(Planets.getByNumber(8));
         break;
       default:
         System.out.println("некорректный ввод");
@@ -71,6 +62,6 @@ public class Main {
     do {
       n = scanner.nextInt();
       printTyp(n);
-    } while (n <= 0);
+    } while (n <= 0 || n > 8);
   }
 }
--- src/homework34/Planets.java
+++ src/homework34/Planets.java
@@ -1,25 +1,31 @@
 package homework34;
 
 public enum Planets {
-  MERCURY("Меркурий", 2440, 3.302e+23),
-  VENUS("Венера", 6052, 4.869e+24),
-  EARTH("Земля", 6378, 5.974e+24),
-  MARS("Марс", 3397, 6.419e+23),
-  JUPITER("Юпитер", 71490, 1.899e+27),
-  SATURN("Сатурн", 60270, 5.685e+26),
-  URANUS("Уран", 25560, 8.685e+25),
-  NEPTUNE("Нептун", 24760, 1.024e+26);
+  MERCURY(1, "Меркурий", 2440, 3.302e+23),
+  VENUS(2, "Венера", 6052, 4.869e+24),
+  EARTH(3, "Земля", 6378, 5.974e+24),
+  MARS(4, "Марс", 3397, 6.419e+23),
+  JUPITER(5, "Юпитер", 71490, 1.899e+27),
+  SATURN(6, "Сатурн", 60270, 5.685e+26),
+  URANUS(7, "Уран", 25560, 8.685e+25),
+  NEPTUNE(8, "Нептун", 24760, 1.024e+26);
 
+  private final int number;
   private final String name;
   private final int radius;
   private final double weight;
-  private final double GRAVITATIONAL_CONSTANT = 6.6743e-11;
+  private static final double GRAVITATIONAL_CONSTANT = 6.6743e-11;
 
 
-  private Planets(String name, int radius, double weight) {
+  private Planets(int number, String name, int radius, double weight) {
+    this.number = number;
     this.name = name;
     this.radius = radius;
     this.weight = weight;
+  }
+
+  public int getNumber() {
+    return number;
   }
 
   public String getName() {
@@ -34,6 +40,15 @@ public enum Planets {
     return weight;
   }
 
+  public static Planets getByNumber(int number) {
+    Planets[] planet = {MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE};
+    for (int i = 0; i < planet.length; i++) {
+      if (planet[i].getNumber() == number) {
+        return planet[i];
+      }
+    }
+    return null;
+  }
 
   public double printAccelerationOfGravity() {
     return ((GRAVITATIONAL_CONSTANT * this.weight) / (this.radius * this.radius)) * 10e-7;
