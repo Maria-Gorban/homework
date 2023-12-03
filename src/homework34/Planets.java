9bb7b5f38d9d110a16b1efa9885525c6e730c1a2
Maria G <mgorban@rheinahrcampus.de>
2023-12-03 13:28:53 +0100

    fix task02

--- src/homework34/Planets.java
+++ src/homework34/Planets.java
@@ -1,31 +1,27 @@
 package homework34;
 
 public enum Planets {
-  MERCURY(1, "Меркурий", 2440, 3.302e+23),
-  VENUS(2, "Венера", 6052, 4.869e+24),
-  EARTH(3, "Земля", 6378, 5.974e+24),
-  MARS(4, "Марс", 3397, 6.419e+23),
-  JUPITER(5, "Юпитер", 71490, 1.899e+27),
-  SATURN(6, "Сатурн", 60270, 5.685e+26),
-  URANUS(7, "Уран", 25560, 8.685e+25),
-  NEPTUNE(8, "Нептун", 24760, 1.024e+26);
+  MERCURY("Меркурий", 2440, 3.302e+23),
+  VENUS("Венера", 6052, 4.869e+24),
+  EARTH("Земля", 6378, 5.974e+24),
+  MARS("Марс", 3397, 6.419e+23),
+  JUPITER("Юпитер", 71490, 1.899e+27),
+  SATURN("Сатурн", 60270, 5.685e+26),
+  URANUS("Уран", 25560, 8.685e+25),
+  NEPTUNE("Нептун", 24760, 1.024e+26);
 
-  private final int number;
+
   private final String name;
   private final int radius;
   private final double weight;
   private static final double GRAVITATIONAL_CONSTANT = 6.6743e-11;
 
 
-  private Planets(int number, String name, int radius, double weight) {
-    this.number = number;
+   Planets(String name, int radius, double weight) {
+
     this.name = name;
     this.radius = radius;
     this.weight = weight;
-  }
-
-  public int getNumber() {
-    return number;
   }
 
   public String getName() {
@@ -38,16 +34,6 @@ public enum Planets {
 
   public double getWeight() {
     return weight;
-  }
-
-  public static Planets getByNumber(int number) {
-    Planets[] planet = {MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE};
-    for (int i = 0; i < planet.length; i++) {
-      if (planet[i].getNumber() == number) {
-        return planet[i];
-      }
-    }
-    return null;
   }
 
   public double printAccelerationOfGravity() {
