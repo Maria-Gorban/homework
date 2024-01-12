890d000210ef48a0c31adc47edd648c49fd1d572
Maria G <mgorban@rheinahrcampus.de>
2024-01-12 11:08:05 +0100

    fix task02

--- src/homework46/Task02.java
+++ src/homework46/Task02.java
@@ -3,16 +3,8 @@ package homework46;
 import java.util.Scanner;
 
 public class Task02 {
-
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter number of a day of the week: ");
-    int number;
-    do {
-      number = scanner.nextInt();
-    } while (number < 1 || number > 7);
-
-    switch (number) {
+  public static void printDayWeek(int n) {
+    switch (n) {
       case 1:
         System.out.println("Monday");
         break;
@@ -34,7 +26,16 @@ public class Task02 {
       case 7:
         System.out.println("Sunday");
         break;
-
     }
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int number;
+    do {
+      System.out.print("Enter number of a day of the week: ");
+      number = scanner.nextInt();
+    } while (number < 1 || number > 7);
+    printDayWeek(number);
   }
 }
