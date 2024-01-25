2c9509fbf48063368b6659b8f52c6a3d55666088
Maria G <mgorban@rheinahrcampus.de>
2024-01-25 12:52:35 +0100

    add task with tests for homework51

--- src/homework51/task01.java
+++ src/homework51/task01.java
@@ -1,12 +1,14 @@
 package homework51;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
 import static java.lang.Character.toUpperCase;
 
 public class task01 {
 
 
-  public static boolean isPalindrome(String word) {
+  public static boolean isPalindromeIgnoresSpaces(String word) {
 
     char[] signs = {',', '.', ';', ':', '!', '?', '-'};
 
@@ -28,12 +30,10 @@ public class task01 {
     return true;
   }
 
-  /*
   public static boolean isPalindrome(String word) {
-  word = word.toLowerCase().replace("[^a-z]", "");
-  return reverse(word).equals(word);
+    word = word.toLowerCase().replace("[^a-z]", "");
+    return reverse(word).equals(word);
   }
-   */
 
   public static boolean isPangram(String sentence) {
     sentence = sentence.replaceAll("\\s", "");
@@ -114,6 +114,7 @@ public class task01 {
   }
 
   public static int countOccurencies(String text, char letter) {
+    text = text.toLowerCase();
     if (text == null || text.isEmpty()) {
       return 0;
     }
@@ -157,16 +158,21 @@ public class task01 {
     return sb.toString();
   }
 
+
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+
+    System.out.println(Arrays.toString(parseIp("123.6.89.222")));
     System.out.println("Enter your text for isPalindrome: ");
     System.out.println(isPalindrome(scanner.nextLine()));
 
     System.out.println("Enter your text for isPangram: ");
     System.out.println(isPangram(scanner.nextLine()));
 
-    System.out.println("Enter your text to encrypt it: ");
-    System.out.println(encrypt(scanner.nextLine()));
+
+    System.out.println(encrypt("Hello World"));
+
+    System.out.println(countOccurencies("one two three four five", 'z'));
 
 
   }
